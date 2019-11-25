931de2d5c69d6023dd37ddcdb5f3c0aa2fef7713
reda-lamtaoui <56556458+reda-lamtaoui@users.noreply.github.com>
2019-11-25 18:20:00 +0100

    Update index.php

--- FORUM/index.php
+++ FORUM/index.php
@@ -1,20 +1,40 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
 <link rel="icon" type="image/jpeg" href="images/Logo_Lon.jpeg"> 
-<title>LON | Traveaux & Devis</title>
+<title>Forum</title>
 <meta charset="utf-8">
 <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
 </head>
 <body>
- <?php
-include 'BD.php';
+	<?php
+	include 'BD.php';
 include 'user.php';
+	 if(isset($_POST['username2'])){
+        $UserConnecte = new User();
+        $UserConnecte->login($_POST['username2'],$_POST['password']);
+        $_SESSION["id"]=$UserConnecte->getId();
+       $_SESSION["name"]=$UserConnecte->getName();
+       $_SESSION["username"]=$UserConnecte->getUsername();
+    }
+     
+?>
+	<?php
+if(isset($_POST['deco'])){
+  session_destroy();
+  session_start();
+     }
+?>
+ <?php
+
 if(isset($_POST['name'])){
 	if ($_POST['civilite']=="Mr") {
-		$avatar="bw2 - Copie.jpg";
+		$avatar="1.jpg";
 	}else{
-		$avatar="bw2 - Copie.jpg";
+		$avatar="2.jpg";
 	}
         //get username and password
         $data =array("name" =>$_POST['name'], 
@@ -27,7 +47,7 @@ if(isset($_POST['name'])){
               "joinDate"=>date("d-m-Y")
       );
         //Create user object
-        $user = new User;
+        $user = new User();
         $user->createUser($data);
      }
 ?>
@@ -38,21 +58,26 @@ if(isset($_POST['name'])){
 						<li id="nav2"><a>A propos<span><b>FORUM</b></span></a></li>
 						<li id="nav3"><a href="Services">Sujet<span><b>pour vous</b></span></a></li>
 						<!--Contacts--> <li id="nav5"><a href="">Contacts<span><b></b></span></a></li>
+						<?php 
+						if(isset($_SESSION['id'])){
+							?>
+							<li id="nav6"><a onclick="$(document).ready(function(){
+        								profilogin();
+    									})">reda<span><b>Profil</b></span></a></li>
+							<?php
+						}else{
+							?>
 										<li id="nav6"><a onclick="$(document).ready(function(){
         								userlogin();
     									})">Login<span><b>Sign Up</b></span></a></li>
+    						<?php
+    					} ?>
 					</ul>
 				</nav>
 		
 		
 	</div>
 	<?php include('login.php'); ?>
-	 <?php
-	 if(isset($_POST['username'])){
-        $usern = new User;
-        $usern->login($_POST['username'],$_POST['password']);
-    }
-     
-?>
+	<?php include('profil.php'); ?>
 </body>
 </html>

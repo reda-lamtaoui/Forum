7920baa7d168a1610d0e02acbe56806b9b873d9c
reda-lamtaoui <56556458+reda-lamtaoui@users.noreply.github.com>
2019-12-15 16:14:55 +0100

    Update profil.php

--- FORUM/profil.php
+++ FORUM/profil.php
@@ -1,63 +1,182 @@
-<!doctype html>
-<html lang="en">
-
+<?php
+session_start();
+include 'Class/model/BD.php';
+include 'Class/model/user.php';
+include 'Class/model/categorie.php';
+include 'Class/model/topic.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title></title>
+<link rel="icon" type="image/jpeg" href="images/Logo_Lon.jpeg"> 
+<title>Forum</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all"> 
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400" rel="stylesheet" />    
+  <link href="css/templatemo-style.css" rel="stylesheet" />
+  <style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 900px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+}
 
-    <link rel="shortcut icon" href="login_assets/images/fav.jpg">
-<link rel="stylesheet" href="login_assets/css/bootstrap.min.css" type="text/css" media="all">
-<link rel="stylesheet" href="login_assets/css/fontawsom-all.min.css" type="text/css" media="all">
-<link rel="stylesheet" href="login_assets/css/style1.css" type="text/css" media="all">
-<script type="text/javascript" src="login_assets/js/jquery-3.2.1.min.js" ></script>
-<script type="text/javascript" src="login_assets/js/popper.min.js" ></script>
-<script type="text/javascript" src="login_assets/js/bootstrap.min.js" ></script>
-<script type="text/javascript" src="login_assets/js/script.js" ></script>
+.title {
+  color: grey;
+  font-size: 18px;
+}
+
+button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+a {
+  text-decoration: none;
+  font-size: 22px;
+  color: black;
+}
+
+button:hover, a:hover {
+  opacity: 0.7;
+}
+</style>
 </head>
+<body>
+  <?php
+   if(isset($_POST['connexion'])){
+        $UserConnecte = new User();
+        $UserConnecte->login($_POST['username2'],$_POST['password']);
+        $_SESSION["id"]=$UserConnecte->getId();
+         $_SESSION["connexion"]="";
+    }
+if(isset($_POST['deco'])){
+  session_destroy();
+  session_start();
+     }
 
+?>
+<!--  <div class="body1">
+        <nav>
+          <ul id="menu">
+            <li id="nav1" class="active"><a href="/">Acceuil<span><b>Bienvenue!</b></span></a></li>
+            <li id="nav2"><a>A propos<span><b>FORUM</b></span></a></li>
+            <li id="nav3"><a href="Services">Sujet<span><b>pour vous</b></span></a></li>
+             <li id="nav5"><a href="">Contacts<span><b></b></span></a></li>
+            <?php 
+            if(isset($_SESSION['id'])){
+              ?>
+              <li id="nav6"><a onclick="$(document).ready(function(){
+                        profilogin();
+                      })">reda<span><b>Profil</b></span></a></li>
+              <?php
+            }else{
+              ?>
+                    <li id="nav6"><a onclick="$(document).ready(function(){
+                        userlogin();
+                      })">Login<span><b>Sign Up</b></span></a></li>
+                <?php
+              } ?>
+          </ul>
+        </nav>
     
-            <div id="profil" class="modal" tabindex="-1" role="dialog">
-              <div class="modal-dialog tem-doc" role="document">
-                <div class="modal-content temp-model">
-                  <div class="modal-header">
-                    <h5 class="modal-title"><?php echo "".$_SESSION["name"]; ?></h5> <br>
-                    <p><?php echo "".$_SESSION["username"]; ?></p>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <form action="" method="Post" method="Post" name="eval" enctype="multipart/form-data" >
-                    
-                  <div class="modal-body">
-                    
-                       <div class="row form-ro">
-                           <p>derniere connexion:</p>
-                           <input name="deco" class="form-control" type="hidden">
-                       </div>
-                         <div id="message">
-                        </div>
-                        <div class="row form-ro">
-                            <a onclick="" class="btn btn-danger">Paramétre</a>
-                       </div>
-                       <div class="row form-ro">
-                            <input type="submit" value="deconnexion" class="btn btn-success">
-                       </div>
-                       </form>
-                  </div>
+    
+  </div> -->
+  
+<div class="container">
+  <!-- Top box -->
+    <!-- Logo & Site Name -->
+    <div class="placeholder">
+      <div class="parallax-window" data-parallax="scroll" data-image-src="img/simple-house-01.jpg">
+        <div class="tm-header">
+          <div class="row tm-header-inner">
+            <div class="col-md-6 col-12">
+              <img src="img/simple-house-logo.png" alt="Logo" class="tm-site-logo" /> 
+              <div class="tm-site-text-box">
+                <h1 class="tm-site-title">Ader Forum</h1>
+                <h6 class="tm-site-description">new Topics everyday</h6>  
               </div>
             </div>
+            <nav class="col-md-6 col-12 tm-nav">
+              <ul class="tm-nav-ul">
+                <li class="tm-nav-li"><a href="index.php" class="tm-nav-link active">Home</a></li>
+                <li class="tm-nav-li"><a href="about.php" class="tm-nav-link">About</a></li>
+                <li class="tm-nav-li"><a href="contact.php" class="tm-nav-link">Contact</a></li>
+                <?php 
+            if(isset($_SESSION['connexion'])){
+              ?>
+              <li class="tm-nav-li"><a href="topic" class="tm-nav-link">Profil</a></li>
+              <?php
+            }else{
+              ?>
+                    <li class="tm-nav-li"><a onclick="$(document).ready(function(){
+                        userlogin();
+                      })" class="tm-nav-link">Signup/signin</a></li>
+                <?php
+              } ?>
+                <!-- <li class="tm-nav-li"><a href="contact.html" class="tm-nav-link">Signup/signin</a></li> -->
+              </ul>
+            </nav>  
+          </div>
         </div>
-    </body>
+      </div>
+    </div>
+    <?php 
+$UserConnecte=new user();
+$UserConnecte->loadUser($_SESSION["id"]);
+?>
+<main>
+      <div class="chemin">
+      <p><img src="img/home.svg">-><a href="index.php">Home</a>-><a href="topic">profil</a></p>
+      </div>
+      <h2 style="text-align:center">User Profile</h2>
 
+<div class="card">
+  <?php echo '<img src="'.$UserConnecte->getAvatar().'" style="width:20%;margin-left: auto;
+  margin-right: auto;">'; ?>
+  <h1><?php echo "".$UserConnecte->getName(); ?></h1>
+  <p class="title"><?php echo "".$UserConnecte->getAbout(); ?></p>
+  <p>Last connexion: <?php echo "".$UserConnecte->getLastActivity(); ?></p>
+  <p><a href="Modifier.php"><button>Modifier</button></a></p>
+</div>
+    </main>
+    
 
-
-<script>
-
-  
-    function profilogin(){
-        $("#profil").modal('show');   
-    }
-</script>
-
+    <footer class="tm-footer text-center">
+      <p>Copyright &copy; 2020 Simple House 
+            
+            | Design: <a rel="nofollow" href="https://templatemo.com">TemplateMo</a></p>
+    </footer>
+  </div>
+<script src="js/jquery.min.js"></script>
+  <script src="js/parallax.min.js"></script>
+  <script>
+    $(document).ready(function(){
+      // Handle click on paging links
+      $('.tm-paging-link').click(function(e){
+        e.preventDefault();
+        
+        var page = $(this).text().toLowerCase();
+        $('.tm-gallery-page').addClass('hidden');
+        $('#tm-gallery-page-' + page).removeClass('hidden');
+        $('.tm-paging-link').removeClass('active');
+        $(this).addClass("active");
+      });
+    });
+  </script>
+<?php include('login.php'); ?>
+  <?php include('profil2.php'); ?>
+</body>
 </html>
